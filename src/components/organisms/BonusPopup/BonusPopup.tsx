12195823d8d94270fa8822a11ff7ad73cff4c16e
Denis Bortnikov <denis.bortnikov17@gmail.com>
2024-03-02 22:32:46 +0300

    feat: finish bonus item

--- src/components/organisms/BonusPopup/BonusPopup.tsx
+++ src/components/organisms/BonusPopup/BonusPopup.tsx
@@ -7,13 +7,23 @@ export const BonusPopup: FC = () => (
     <span className='bonus__title'>Deposit and play</span>
 
     <button className='bonus__item'>
-      <span className='bonus__item__bg' onClick={() => console.log("tut")}></span>
+      <span className='bonus__item__bg'></span>
       <div className='bonus__item__content'>
-        <p className='bonus__item__content__title'>Welcome bonus</p>
-        <p className='bonus__item__content__desc'>
-          100% up to €500 <br />
-          +120 FreeSpins
-        </p>
+        <div className='bonus__item__content__image'>
+          <img src='./assets/images/bonus/welcome.png' alt='welcome' />
+        </div>
+
+        <div className='bonus__item__content__text'>
+          <p className='bonus__item__content__text__title'>Welcome bonus</p>
+          <p className='bonus__item__content__text__desc'>100% up to €500 +120 FreeSpins</p>
+        </div>
+
+        <div className='bonus__item__content__toggle'>
+          <img src='./assets/images/bonus/vertical-line.png' alt='vertical-line' />
+          <div className='bonus__item__content__toggle__vector'>
+            <img src='./assets/images/bonus/vector.png' alt='vector' />
+          </div>
+        </div>
       </div>
     </button>
 
