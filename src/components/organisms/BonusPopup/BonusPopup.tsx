5af9505fd61c7f42efc4d3dfe2099514abc774c7
Denis Bortnikov <denis.bortnikov17@gmail.com>
2024-03-02 17:46:18 +0300

    feat: start implement bonus item

--- src/components/organisms/BonusPopup/BonusPopup.tsx
+++ src/components/organisms/BonusPopup/BonusPopup.tsx
@@ -6,6 +6,17 @@ export const BonusPopup: FC = () => (
   <div className='bonus'>
     <span className='bonus__title'>Deposit and play</span>
 
+    <button className='bonus__item'>
+      <span className='bonus__item__bg' onClick={() => console.log("tut")}></span>
+      <div className='bonus__item__content'>
+        <p className='bonus__item__content__title'>Welcome bonus</p>
+        <p className='bonus__item__content__desc'>
+          100% up to €500 <br />
+          +120 FreeSpins
+        </p>
+      </div>
+    </button>
+
     <button className='bonus__start'>
       <span className='bonus__start__bg'></span>
       <span className='bonus__start__text'>Start with your 100% bonus</span>
