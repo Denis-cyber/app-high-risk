3db61089abc7453a9fda069f9d5e4f3b07b0c95d
Denis Bortnikov <denis.bortnikov17@gmail.com>
2024-03-06 11:57:40 +0300

    fix: add classnames for menu-btn lines

--- src/components/atoms/MenuButton/MenuButton.tsx
+++ src/components/atoms/MenuButton/MenuButton.tsx
@@ -3,11 +3,11 @@ import { FC } from "react";
 import "./MenuButton.scss";
 
 export const MenuButton: FC = () => (
-  <button className='menu'>
-    <div className='menu__lines'>
-      <span></span>
-      <span></span>
-      <span></span>
+  <button className='menu-btn'>
+    <div className='menu-btn__lines'>
+      <span className='menu-btn__lines__item'></span>
+      <span className='menu-btn__lines__item'></span>
+      <span className='menu-btn__lines__item'></span>
     </div>
   </button>
 );
